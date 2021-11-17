715196299f30877fa0d37991dd1ffe21894883cf
Mario Cruz <cruzm0012@gmail.com>
2021-11-17 17:11:20 -0600

    Initial Commit

---
+++ MWT Personal Project 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MWT Personal Project 2
+//
+//  Created by Central States SER Mac 22 on 11/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MWT Personal Project 2/MWT_Personal_Project_2App.swift
@@ -0,0 +1,17 @@
+//
+//  MWT_Personal_Project_2App.swift
+//  MWT Personal Project 2
+//
+//  Created by Central States SER Mac 22 on 11/17/21.
+//
+
+import SwiftUI
+
+@main
+struct MWT_Personal_Project_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
