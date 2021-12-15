0455462826f4889027fc493e9232f2baebda91ef
Ashley Anjeles <aanjeles0@gmail.com>
2021-12-15 16:18:20 -0600

    Update journal

---
+++ MWT Personal Project 2/JournalHome.swift
@@ -0,0 +1,20 @@
+//
+//  JournalHome.swift
+//  MWT Personal Project 2
+//
+//  Created by Ashley Anjeles on 12/15/21.
+//
+
+import SwiftUI
+
+struct JournalHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct JournalHome_Previews: PreviewProvider {
+    static var previews: some View {
+        JournalHome()
+    }
+}
